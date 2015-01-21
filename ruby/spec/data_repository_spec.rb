a56f0d397dc9c9d7152ec4ac9fad26e4c3410904
Bruno Casali <brunoocasali@gmail.com>
2015-01-21 00:24:24 -0200

    added the expectations inside a spec tests for data_repository

--- ruby/spec/data_repository_spec.rb
+++ ruby/spec/data_repository_spec.rb
@@ -1,6 +1,20 @@
-require 'spec_helper'
 require 'data_repository'
 
 describe DataRepository do
+  context 'when talked about a directories' do
+    it 'is valid if it exists'
+    it "is invalid if isn't exists"
+  end
+
+  context 'when talked about a text data file' do
+    it 'is valid only with have a .txt extension'
+    it "is invalid if doesn't have a .txt extension"
+    it 'is usable if have any data text'
+    it "needs return nil if doesn't have any text data"
+  end
+
+  context 'when talked about a text data inside the file' do
+    it 'needs to have a data in a correct form'
+  end
 end
 
