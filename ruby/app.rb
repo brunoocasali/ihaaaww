21cca45d364a8e285414e376af70d40bda231f9a
Bruno Casali <brunoocasali@gmail.com>
2015-01-26 15:05:08 -0200

    added the beautiful version now. But working in another most pretty than it

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,59 +1,18 @@
+require 'yaml'
+
 # Load all required files to run the app.
 Dir["lib/*.rb"].each { |file| require File.expand_path(file) }
-
-lake_rate_0 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKEND, 90)
-lake_rate_2 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKEND, 80)
-
-lake_rate_1 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKDAYS, 110)
-lake_rate_3 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKDAYS, 80)
-
-bridge_rate_0 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKEND, 60)
-bridge_rate_2 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKEND, 50)
-
-bridge_rate_1 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKDAYS, 160)
-bridge_rate_3 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKDAYS, 110)
-
-ridge_rate_0 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKEND, 150)
-ridge_rate_2 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKEND, 40)
-
-ridge_rate_1 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKDAYS, 220)
-ridge_rate_3 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKDAYS, 100)
-
-tiger_rate_0 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKEND, 200)
-tiger_rate_2 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKEND, 99)
-
-tiger_rate_1 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKDAYS, 198)
-tiger_rate_3 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKDAYS, 91)
-
-robin_rate_0 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKEND, 99)
-robin_rate_2 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKEND, 64)
-
-robin_rate_1 = HotelRate.new(CustomerType::REGULAR, DayType::WEEKDAYS, 199)
-robin_rate_3 = HotelRate.new(CustomerType::REWARDS, DayType::WEEKDAYS, 100)
-
-
-bridge_rates = []; lake_rates = []; ridge_rates = []; tiger_rates = []; robin_rates = []
-
-4.times { |n|
-  lake_rates << eval("lake_rate_#{n}")
-  bridge_rates << eval("bridge_rate_#{n}")
-  ridge_rates << eval("ridge_rate_#{n}")
-  tiger_rates << eval("tiger_rate_#{n}")
-  robin_rates << eval("robin_rate_#{n}")
-}
-
-lake_hotel = Hotel.new('Lakewood', 3, lake_rates)
-bridge_hotel = Hotel.new('Bridgewood', 4, bridge_rates)
-ridge_hotel = Hotel.new('Ridgewood', 5, ridge_rates)
-tiger_hotel = Hotel.new('TigerWood', 6, tiger_rates)
-robin_hotel = Hotel.new('RobinWood', 4, robin_rates)
 
 data_repository = DataRepository.new
 data_repository.read
 
-array = [lake_hotel, bridge_hotel, ridge_hotel, tiger_hotel, robin_hotel]
+hotels = []
+$/="\n\n"
+File.open('data/objects.yaml', 'r').each do |object|
+  hotels << YAML::load(object)
+end
 
-finder = HotelFinder.new(array)
+finder = HotelFinder.new(hotels.flatten!)
 
 data_repository.text_data.each do |line|
   customer = CustomerBuilder.create(line)
